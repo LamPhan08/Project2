700f0f5a9a6e30a7dc6882b57724340eb76d21c6
LamPhan08 <nhatlampr@gmail.com>
2023-10-20 20:55:57 +0700

    Add header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import './header.css'
+import userIcon from '../../assets/images/user.png'
+import driveIcon from '../../assets/images/Drive.png'
+import SearchIcon from '@mui/icons-material/Search';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu)
+  }
+
+  return (
+    <div className="header">
+      <div className="logo_container">
+        <img src={driveIcon} alt="" />
+
+        <span>Drive</span>
+      </div>
+
+      <div className="searchBarContainer">
+        <div className="searchBar">
+          <SearchIcon className='icon' />
+
+          <input type="text" placeholder='Search in Drive' />
+        </div>
+      </div>
+
+      <div className="avatarContainer" onClick={() => handleOpenMenu()}>
+        <img src={userIcon} alt="" />
+
+        <span>Username</span>
+
+
+      </div>
+
+      <div className={`dropdown_menu ${openMenu ? 'active' : 'inactive'}`}>
+          <div className='user_in4_container'>
+            <h6>example@gmail.com</h6>
+
+            <img src={userIcon} alt="" />
+          </div>
+
+          <div className="btn_zone">
+            <LogoutIcon />
+
+            <span>Logout</span>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
